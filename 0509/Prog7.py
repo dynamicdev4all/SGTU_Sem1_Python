45260658ae20bbb4ecf8416c8bc2ec382d6202ba
dynamicdev4all <149931467+dynamicdev4all@users.noreply.github.com>
2024-09-05 12:16:28 +0530

    Add files via upload

---
+++ 0509/Prog7.py
@@ -0,0 +1,28 @@
+# create a dictionary
+#  -name
+#  -age
+#  -roll no
+
+# user input : num 
+#   - num is odd
+#       then print name
+    # -num is even
+    #     then print rollno
+
+my_dic = {
+    "name" : "Gita",
+    "age" : 62,
+    "rollno" : 12345
+}
+
+num1 = int(input("Enter num"))
+
+# if (conditon)
+# even
+if num1 % 2 == 0:
+    print(my_dic["rollno"])
+
+# odd
+# if num1 % 2 == 1:
+else:
+    print(my_dic["name"])
---
+++ 0509/Prog8.py
@@ -0,0 +1,11 @@
+# User input head or tail
+# head : win
+# tail : loose
+
+print("choose head or tail")
+choice = input("Enter choice")
+
+if(choice == "head"):
+    print("U won")
+if(choice == "tail"):
+    print("U lose")
